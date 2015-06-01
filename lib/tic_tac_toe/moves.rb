ae47087ba86271adceb0979e5cd130ff35f5ff3d
rickstewart <rick.stewart@ns.sympatico.ca>
2015-06-01 05:49:20 -0300

    May 31

--- lib/tic_tac_toe/moves.rb
+++ lib/tic_tac_toe/moves.rb
@@ -2,11 +2,11 @@ class Moves
 
   ALLOWED = (1..9)
   WINNING_MOVES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-  @remaining_moves = ALLOWED.to_a
+  @@remaining_moves = ALLOWED.to_a
 
   def self.valid_move?(square)
-    if ALLOWED.include?(square) && @remaining_moves.include?(square)
-      @remaining_moves.delete square
+    if ALLOWED.include?(square) && @@remaining_moves.include?(square)
+      @@remaining_moves.delete square
       true
     else
       false
@@ -14,7 +14,7 @@ class Moves
   end
 
   def self.valid_moves
-    @remaining_moves
+    @@remaining_moves
   end
 
   def self.winning_move?(moves)
