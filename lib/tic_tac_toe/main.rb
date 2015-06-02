3ca65ea0ae24a4826d709d0d4f203f8cd03208f7
rickstewart <rick.stewart@ns.sympatico.ca>
2015-06-02 17:59:18 -0300

    June 2

--- lib/tic_tac_toe/main.rb
+++ lib/tic_tac_toe/main.rb
@@ -12,33 +12,46 @@ class Main
     puts "Player 2, please enter your name: "
     name = gets.chomp
     player2 = Player.new(name, "O")
-
     while true
-      puts "#{player1.player_name} (you'll be X), its your move. Pick a square: "
-      move = gets.chomp.to_i
-      if Moves.valid_move?(move)
-        board.update_square(move, player1.game_piece)
-        player1.add_move(move)
-        board.draw_board
-        if Moves.winning_move? player1.player_moves
-          puts player1.player_name + " Wins!"
+      while true
+        puts "#{player1.player_name} (you'll be X), its your move. Pick a square: "
+        move = gets.chomp.to_i
+        if Moves.valid_move?(move)
+          board.update_square(move, player1.game_piece)
+          player1.add_move(move)
+          board.draw_board
           break
+        else
+          puts "Illegal move! Please pick one of the remaining possible moves: " + Moves.valid_moves.to_s
         end
-      else
-        puts "Illegal move! Please pick one of the remaining possible moves: " + Moves.valid_moves.to_s
       end
-      puts "#{player2.player_name} (you'll be O), its your move. Pick a square: "
-      move = gets.chomp.to_i
-      if Moves.valid_move?(move)
-        board.update_square(move, player2.game_piece)
-        player2.add_move(move)
-        board.draw_board
-        if Moves.winning_move? player2.player_moves
-          puts player2.player_name + " Wins!"
+      if Moves.winning_move? player1.player_moves
+        puts player1.player_name + " Wins! Game over..."
+        break
+      end
+      if Moves.valid_moves.size ==0
+        "Its a tie! Game over..."
+        break
+      end
+      while true
+        puts "#{player2.player_name} (you'll be X), its your move. Pick a square: "
+        move = gets.chomp.to_i
+        if Moves.valid_move?(move)
+          board.update_square(move, player2.game_piece)
+          player2.add_move(move)
+          board.draw_board
           break
+        else
+          puts "Illegal move! Please pick one of the remaining possible moves: " + Moves.valid_moves.to_s
         end
-      else
-        puts "Illegal move! Please pick one of the remaining possible moves: " + Moves.valid_moves.to_s
+      end
+      if Moves.winning_move? player2.player_moves
+        puts player2.player_name + " Wins! Game over..."
+        break
+      end
+      if Moves.valid_moves.size ==0
+        "Its a tie! Game over..."
+        break
       end
     end
   end
