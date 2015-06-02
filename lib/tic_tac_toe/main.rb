4cf8c97db03e257e0da993adc675a2c7dceb7f35
rstewart <rick.stewart@ns.sympatico.ca>
2015-06-02 14:16:54 -0300

    June 2

--- lib/tic_tac_toe/main.rb
+++ lib/tic_tac_toe/main.rb
@@ -12,20 +12,34 @@ class Main
     puts "Player 2, please enter your name: "
     name = gets.chomp
     player2 = Player.new(name, "O")
-    # loop until game is finished
+
     while true
-       puts "#{player1.player_name} (you'll be X), its your move. Pick a square: "
-       move = gets.chomp.to_f
-       if Moves.valid_move?(move)
-         board.update_square(move, player1.game_piece)
+      puts "#{player1.player_name} (you'll be X), its your move. Pick a square: "
+      move = gets.chomp.to_i
+      if Moves.valid_move?(move)
+        board.update_square(move, player1.game_piece)
         player1.add_move(move)
-         board.draw_board
-         if Moves.winning_move?player1.player_moves
-           return player1.player_name + " Wins!"
-         end
-       else
-        puts "Illegal move! Please pick one of the remaining possible moves: " + Moves.valid_moves
-       end
-     end
+        board.draw_board
+        if Moves.winning_move? player1.player_moves
+          puts player1.player_name + " Wins!"
+          break
+        end
+      else
+        puts "Illegal move! Please pick one of the remaining possible moves: " + Moves.valid_moves.to_s
+      end
+      puts "#{player2.player_name} (you'll be O), its your move. Pick a square: "
+      move = gets.chomp.to_i
+      if Moves.valid_move?(move)
+        board.update_square(move, player2.game_piece)
+        player2.add_move(move)
+        board.draw_board
+        if Moves.winning_move? player2.player_moves
+          puts player2.player_name + " Wins!"
+          break
+        end
+      else
+        puts "Illegal move! Please pick one of the remaining possible moves: " + Moves.valid_moves.to_s
+      end
+    end
   end
 end
--- lib/tic_tac_toe/moves.rb
+++ lib/tic_tac_toe/moves.rb
@@ -1,5 +1,7 @@
 class Moves
 
+  require "set"
+
   ALLOWED = (1..9)
   WINNING_MOVES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
   @@remaining_moves = ALLOWED.to_a
@@ -18,6 +20,6 @@ class Moves
   end
 
   def self.winning_move?(moves)
-  WINNING_MOVES.any? {|win| win - moves == 0}
+  WINNING_MOVES.any? {|win| Set.new(win) == Set.new(moves)}
   end
 end
