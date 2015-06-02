eeb120bbd0aa502611891ccff63275722d8dcb17
rickstewart <rick.stewart@ns.sympatico.ca>
2015-06-02 05:43:27 -0300

    June 2

--- lib/tic_tac_toe/main.rb
+++ lib/tic_tac_toe/main.rb
@@ -1,31 +1,31 @@
 class Main
-  require "game_board"
-  require "moves"
-  require "player"
+  require_relative "game_board"
+  require_relative "moves"
+  require_relative "player"
 
   def go
     board = GameBoard.new
     board.draw_board
     puts "Player 1, please enter your name: "
-    name = gets name.chomp
+    name = gets.chomp
     player1 = Player.new(name, "X")
     puts "Player 2, please enter your name: "
-    name = gets name.chomp
+    name = gets.chomp
     player2 = Player.new(name, "O")
     # loop until game is finished
-    while true
-      puts "#{player1.player_name} -playing X- its your move. Pick a square: "
-      move = gets move
-      if Moves.valid_move?(move)
-        baord.update_square(move,player1.game_piece)
+    # while true
+       puts "#{player1.player_name} (you'll be X), its your move. Pick a square: "
+       move = gets.chomp.to_f
+       if Moves.valid_move?(move)
+         board.update_square(move, player1.game_piece)
         player1.add_move(move)
-        board.draw_board
-        if Moves.winning_move?player1.player_moves
-          return player1.player_name + " Wins!"
-        end
-      else
-        puts "Illegal move! Please pick one of the remaining possible moves: " + Moves.valid_moves
-      end
-    end
+         board.draw_board
+    #     if Moves.winning_move?player1.player_moves
+    #       return player1.player_name + " Wins!"
+    #     end
+    #   else
+    #     puts "Illegal move! Please pick one of the remaining possible moves: " + Moves.valid_moves
+    #   end
+     end
   end
 end
